555403ece35aa66dea111e9706050a9b9d3a6d27
TeoVincent <artur.wincenciak@gmail.com>
2023-01-05 21:08:20 +0100

    Use the ReSharper CLI CleanupCode GitHub Action

---
+++ UnitTests/DemoUnitTests.cs
@@ -0,0 +1,13 @@
+namespace ReSharperCleanupCodeDemo.UnitTests;
+
+/// <summary>
+/// That is an example of unit test class that we usually do not want to clean up
+/// For example another kind of code that we also do not clean up is a generated code (e.g. some rest client)   
+/// </summary>
+internal class DemoUnitTests
+{
+    public void GIVEN_something_WHEN_something_THEN_something()
+    {
+        throw new NotImplementedException();
+    }
+}
