39f9f9e7f397fe78c9e549271351cd1607561ecf
TeoVincent <artur.wincenciak@gmail.com>
2023-01-07 22:44:04 +0100

    Readme

--- DemoClass.cs
+++ DemoClass.cs
@@ -1,12 +1,17 @@
 namespace ReSharperCleanupCodeDemo;
 
-internal class DemoClass
+internal class DemoClass : IDemoInterface
 {
     private readonly int _privateProperty;
     public int PublicProperty { get; set; }
 
+    public string InterfaceProperty { get; set; }
+
     public DemoClass(int privateProperty) =>
         _privateProperty = privateProperty;
+
+    public void InterfaceMethod() =>
+        Console.WriteLine(nameof(InterfaceMethod));
 
     public int PublicMethod() =>
         _privateProperty;
