a31d9a5f47c259bf3590b8a6f333da6d60808929
TeoVincent Artur Wincenciak <artur.wincenciak@gmail.com>
2024-04-02 19:58:38 +0200

    Cleanup: re-format code by JetBrains CleanupCode Tool

--- UnitTests/DemoUnitTests.cs
+++ UnitTests/DemoUnitTests.cs
@@ -1,13 +1,11 @@
 namespace ReSharperCleanupCodeDemo.UnitTests;
 
 /// <summary>
-/// That is an example of unit test class that we usually do not want to clean up
-/// For example another kind of code that we also do not clean up is a generated code (e.g. some rest client)   
+///     That is an example of unit test class that we usually do not want to clean up
+///     For example another kind of code that we also do not clean up is a generated code (e.g. some rest client)
 /// </summary>
 internal class DemoUnitTests
 {
-    public void GIVEN_something_WHEN_something_THEN_something()
-    {
+    public void GIVEN_something_WHEN_something_THEN_something() =>
         throw new NotImplementedException();
-    }
 }
