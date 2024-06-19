881e628f56b980d3ff04f3f02ffcc4414eae532a
kk <kirill.kostenkov@homagames.com>
2024-06-19 14:07:32 +0200

    Add violating code

--- DemoClass.cs
+++ DemoClass.cs
@@ -7,6 +7,7 @@ internal class DemoClass : IDemoInterface
     public string InterfaceProperty { get; set; }
 
     public int PublicProperty { get; set; }
+    public int _someFoo;
 
     public DemoClass(int privateProperty) =>
         _privateProperty = privateProperty;
